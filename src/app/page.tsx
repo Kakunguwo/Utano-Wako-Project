074af9ef6cdc40bf5aa29b3798aa3ac1946a8b36
Ronnie Kakunguwo <ronniekakunguwo03@gmail.com>
2024-11-27 19:52:46 +0200

    ant d removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 
-import { Button } from 'antd';
 import React from 'react';
 
 export default function Home() {
   return (
     <div>
-      <Button type="primary">Button</Button>
+      <h1>Button</h1>
     </div>
   );
 }
